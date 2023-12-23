c8a32eabbbc7c22607920469ebbecefaacf1a5bf
K4mino <erzhan.kz1923@gmail.com>
2023-12-24 01:37:16 +0600

    fix

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -9,14 +9,14 @@ import Header from '../components/Header'
 const LoginPage: React.FC = () => {
     const emailRegExp = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
 
-    const contextValue = React.useContext(EmailContext);
+    const contextValue = React.useContext(EmailContext)
 
     if (contextValue === null) {
-      throw new Error('Email context value is null');
+        throw new Error('Email context value is null')
     }
-    
-    const { email, setEmail }: { email: string; setEmail: (email: string) => void } = contextValue;
-    const timerRef:any = React.useRef(null)
+
+    const { email, setEmail }: { email: string; setEmail: (email: string) => void } = contextValue
+    const timerRef: any = React.useRef(null)
     const [isEmailValid, setIsEmailValid] = React.useState(false)
     const [timer, setTimer] = React.useState(500)
     const [isChecked, setIsChecked] = React.useState(false)
@@ -27,50 +27,92 @@ const LoginPage: React.FC = () => {
     }, [])
 
     React.useEffect(() => {
-      setIsEmailValid(emailRegExp.test(email));
-    },[email])
+        setIsEmailValid(emailRegExp.test(email))
+    }, [email])
 
     const handleMouseDown = () => {
         timerRef.current = setInterval(() => {
-          setTimer((prevTimer:number) => prevTimer - 10);
-        }, 10);
-      };
+            setTimer((prevTimer: number) => prevTimer - 10)
+        }, 10)
+    }
 
-      const handleMouseUp = () => {
-
+    const handleMouseUp = () => {
         if (timer < 0 && isEmailValid && isChecked) {
-          sessionStorage.setItem('email', email);
-          history.push('/step2');
+            sessionStorage.setItem('email', email)
+            history.push('/step2')
         }
         if ((timer < 0 || timer > 0) && !isEmailValid && isChecked) {
-          alert("wrong email");
+            alert('wrong email')
         }
 
         if (timer < 0 && !isChecked && isEmailValid) {
-          alert("Please check the box");
+            alert('Please check the box')
         }
 
         if (timer > 0 && isChecked && isEmailValid) {
-          alert("Fail to hold");
+            alert('Fail to hold')
         }
-
 
         if (timer > 0 && !isChecked && !isEmailValid) {
-          alert("Please complete the form");
+            alert('Please complete the form')
         }
-    
-        clearInterval(timerRef.current);
-        setTimer(500);
-      };
+
+        clearInterval(timerRef.current)
+        setTimer(500)
+    }
+
+    const handleTouchStart = () => {
+        timerRef.current = setInterval(() => {
+            setTimer((prevTimer:number) => prevTimer - 10)
+        }, 10)
+    }
+
+    const handleTouchEnd = () => {
+        if (timer < 0 && isEmailValid && isChecked) {
+            sessionStorage.setItem('email', email)
+            history.push('/step2')
+        }
+
+        if ((timer < 0 || timer > 0) && !isEmailValid && isChecked) {
+            alert('Wrong email')
+        }
+
+        if (timer < 0 && !isChecked && isEmailValid) {
+            alert('Please check the box')
+        }
+
+        if (timer > 0 && isChecked && isEmailValid) {
+            alert('Fail to hold')
+        }
+
+        if (timer > 0 && !isChecked && !isEmailValid) {
+            alert('Please complete the form')
+        }
+
+        clearInterval(timerRef.current)
+        setTimer(500)
+    }
 
     return (
         <>
             <Header />
             <main className="flex flex-col p-4 h-full">
-                <FormInput value={email} onChange={(e: React.ChangeEvent<HTMLInputElement>) => setEmail(e.target.value)} />
+                <FormInput
+                    value={email}
+                    onChange={(e: React.ChangeEvent<HTMLInputElement>) => setEmail(e.target.value)}
+                />
                 <div className="p-1"></div>
-                <FormCheckbox checked={isChecked} onChange={(e: React.ChangeEvent<HTMLInputElement>) => setIsChecked(e.target.checked)}/>
-                <button className="btn btn-primary mt-auto" onMouseDown={handleMouseDown} onMouseUp={handleMouseUp}>
+                <FormCheckbox
+                    checked={isChecked}
+                    onChange={(e: React.ChangeEvent<HTMLInputElement>) => setIsChecked(e.target.checked)}
+                />
+                <button
+                    className="btn btn-primary mt-auto"
+                    onTouchStart={handleTouchStart}
+                    onTouchEnd={handleTouchEnd}
+                    onMouseDown={handleMouseDown}
+                    onMouseUp={handleMouseUp}
+                >
                     Hold to proceed
                 </button>
             </main>
